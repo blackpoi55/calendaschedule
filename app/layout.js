8ba5e935ee213eb422ca6dd7af38f69d1b111739
blackpoi55 <blackpoi55@gmail.com>
2025-09-22 10:55:31 +0700

    +

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={sarabun.className}>
-        {children}
+        {children} 
       </body>
     </html>
   );
